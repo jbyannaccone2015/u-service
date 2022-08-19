4c556cd36eb9fbf33c1e207c6f21d287e578b9cd
jby <justin@yannaccone.net>
2022-08-19 13:04:22 -0400

    Port update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const PORT = process.env.PORT || "5000"
 app.use(cors({ origin: ["http://localhost:3000"], credentials: true}))
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vmuwpnk.mongodb.net/gkinfo`)
@@ -13,5 +14,5 @@ app.use(express.json());
 app.use('/user', require('./routes/userRouter'))
 
 app.listen(process.env.PORT, () => {
-    console.log(`The server is running on ${process.env.PORT}`)
+    console.log(`The server is running on ${PORT}`)
 })
