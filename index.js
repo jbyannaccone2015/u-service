8f2cec3b0ad8f93785d8d46344dfa6f31694f1bd
jby <justin@yannaccone.net>
2022-08-23 11:29:27 -0400

    revoked rework

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 require('dotenv').config()
 const mongoose = require('mongoose')
 const cors = require('cors')
-// app.use(cors({ origin: ["http://localhost:3000"], credentials: true}))
-// app.use(cors({ origin: ["https://gatekeep.netlify.app/"], credentials: true}))
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true}))
+app.use(cors({ origin: ["https://gatekeep.netlify.app/"], credentials: true}))
 
 app.options('*', cors())
 
