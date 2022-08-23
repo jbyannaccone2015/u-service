52cb1eedd24b45fbdb984efc4a41cb9eab23f975
jby <justin@yannaccone.net>
2022-08-23 14:12:37 -0400

    cors implemented

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const mongoose = require('mongoose')
-const cors = require('cors')
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true}))
-app.use(cors({ origin: ["https://gatekeep.netlify.app/"], credentials: true}))
+// const cors = require('cors')
+// app.use(cors({ origin: ["http://localhost:3000"], credentials: true}))
+// app.use(cors({ origin: ["https://gatekeep.netlify.app/"], credentials: true}))
 
-app.options('*', cors())
+// app.options('*', cors())
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vmuwpnk.mongodb.net/gkinfo`)
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,8 @@
 const { getUser, updateUser, deleteUser, createUser } = require('../controller/userController')
 const authMiddleware = require('../middleware/auth')
-const corsMiddleware = require('../middleware/cors')
 
 const router = require('express').Router()
 router.use(authMiddleware)
-router.use(corsMiddleware)
 
 router.get('/:id', getUser)
 router.post('/', createUser)
